dfd70a8797cf1af92990077bb601c1e840230551
as3eem <as3eem@gmail.com>
2017-08-04 11:32:08 +0530

    updated form

--- done.php
+++ done.php
@@ -1,31 +1,30 @@
 <?php
 	$name= $_POST['name'];
 	$phone=	$_POST['phone'];
-	echo "name:".$name." phone: ".$phone;
-
-
+	$email= $_POST['email'];
+	//=========================================
 	$server="localhost";
 	$user="root";
 	$password="Contrasena";
 	$db="formData";
+	date_default_timezone_set("Asia/Calcutta");
+	echo "Hola! ".$name." it's ".date("d/m/Y H:i:s");
 
-
+	//=========================================
 	$connect = mysqli_connect($server,$user,$password,$db);
 	if ($connect->connect_error) {
         die('Error : ('. $connect->connect_errno .') '. $connect->connect_error);
-    
     } 
-	
-	
-    
-
+    //==========================================
     $stat = $connect->prepare('insert into data(`name`,`phone`) VALUES(?, ?)');
-
+    
     $stat->bind_param("ss", $name, $phone);
 
 	if($stat->execute())
 		{
-			echo "hola ".$name;
+			echo "<script>alert('executed')</script>";
+
+
 		}
 	
 
