c28f91a63460b7d32aaa63f4da3b3073e7841762
as3eem <as3eem@gmail.com>
2017-08-09 11:00:44 +0530

    formPHP

---
+++ formPHP/done.php
@@ -0,0 +1,30 @@
+<?php
+	$name= $_POST['name'];
+	$phone=	$_POST['phone'];
+	$email=$_POST['email']
+	//=====================
+	$server="localhost";
+	$user="root";
+	$password="Contrasena";
+	$db="formData";
+	//=====================
+	$connect = mysqli_connect($server,$user,$password,$db);
+	if ($connect->connect_error) {
+        die('Error : ('. $connect->connect_errno .') '. $connect->connect_error);
+    
+    } 
+	
+	
+    
+
+    $stat = $connect->prepare('insert into data(`name`,`phone`) VALUES(?, ?)');
+
+    $stat->bind_param("ss", $name, $phone);
+
+	if($stat->execute())
+		{
+			echo "hola ".$name;
+		}
+	
+
+?>
